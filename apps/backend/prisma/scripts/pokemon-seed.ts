8027e4ecead6cd2cf72c26e580bd529001032ad9
Amine SBAI <ssbaiamine@gmail.com>
2026-04-16 11:38:28 +0100

    fix(backend): configure prisma sqlite adapter

--- apps/backend/prisma/scripts/pokemon-seed.ts
+++ apps/backend/prisma/scripts/pokemon-seed.ts
@@ -1,12 +1,12 @@
 import { PrismaClient } from '@prisma/client';
-// import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
+import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
 
 import TCGdex from '@tcgdex/sdk';
 
-// const connectionString = `${process.env.DATABASE_URL}`;
-
-// const adapter = new PrismaBetterSqlite3({ url: connectionString });
-const prisma = new PrismaClient();
+const adapter = new PrismaBetterSqlite3({
+  url: process.env.DATABASE_URL || 'file:./dev.db',
+});
+const prisma = new PrismaClient({ adapter });
 const tcgdex = new TCGdex('en');
 
 const cardIds = [
