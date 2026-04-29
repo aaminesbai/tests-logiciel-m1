f447de070429d41dc81bee926d90c2d67260798e
OswinFreyr <142348764+OswinFreyr@users.noreply.github.com>
2026-04-29 13:24:32 +0200

    Fix errors from backend (#4)
    * share errors to fix
    
    * remove undefined file
    
    * new error
    
    * fix some errors
    
    * some light fix still not fixing the error (upgrade version of prisma)
    
    * fix(backend): configure prisma sqlite adapter
    
    ---------
    
    Co-authored-by: Amine SBAI <ssbaiamine@gmail.com>

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
@@ -9,6 +11,10 @@ import { CommentsModule } from './comments/comments.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '../.env',
+    }),
     PrismaModule,
     UsersModule,
     CardsModule,
